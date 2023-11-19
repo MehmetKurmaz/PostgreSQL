dc44add0d17c87c21c0a2ff84e868f0a79edac17
Mehmet Kurmaz <m.kurmaz0202@gmail.com>
2023-11-19 19:13:25 +0300

    ödev-10 eklendi

---
+++ ödev-10.sql
@@ -0,0 +1,9 @@
+--1.city tablosu ile country tablosunda bulunan şehir (city) ve ülke (country) isimlerini birlikte görebileceğimiz LEFT JOIN sorgusunu yazınız.
+SELECT city,country FROM City
+LEFT JOIN Country ON city.country_id = country.country_id;
+--2.customer tablosu ile payment tablosunda bulunan payment_id ile customer tablosundaki first_name ve last_name isimlerini birlikte görebileceğimiz RIGHT JOIN sorgusunu yazınız.
+SELECT Payment.payment_id,Customer.first_name,Customer.last_name FROM Payment
+RIGHT JOIN Customer ON Payment.customer_id = Customer.customer_id;
+--3.customer tablosu ile rental tablosunda bulunan rental_id ile customer tablosundaki first_name ve last_name isimlerini birlikte görebileceğimiz FULL JOIN sorgusunu yazınız.
+SELECT Rental.rental_id,Customer.first_name,Customer.last_name FROM Customer
+FULL JOIN Rental ON Rental.customer_id = Customer.customer_id;
